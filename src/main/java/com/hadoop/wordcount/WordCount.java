a5754ea0332c3492c57c66160ab0718fdb6d3c9d
Paulo Monteiro <paulo.from.portugal@gmail.com>
2014-11-21 00:39:33 +0000

    Adding GenericOptionsParser

--- src/main/java/com/hadoop/wordcount/WordCount.java
+++ src/main/java/com/hadoop/wordcount/WordCount.java
@@ -8,13 +8,26 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
+/**
+ * 
+ * To run from within Eclipse in a stand-alone mode, provide the following arguments: <br> 
+ * assets/wordcount/input assets/wordcount/output <br>
+ * 
+ * @author pmonteiro
+ *
+ */
 public class WordCount {
 
 	public static void main(String[] args) throws Exception {
+		
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "WordCount");
-		job.setJarByClass(WordCount.class);
+		String[] folders = new GenericOptionsParser(conf, args).getRemainingArgs();
+        Job job = new  Job(conf, "WordCount");
+        job.setJarByClass(WordCount.class);
+		
+		
 
 		job.setMapperClass(WordCountMapper.class);
 		job.setReducerClass(WordCountReducer.class);
@@ -22,10 +35,10 @@ public class WordCount {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
-		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileInputFormat.setInputPaths(job, new Path(folders[0]));
 		FileSystem fs = FileSystem.get(conf);
 		fs.delete(new Path(args[1]), true); 
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(folders[1]));
 
 		if (!job.waitForCompletion(true))
 			return;
